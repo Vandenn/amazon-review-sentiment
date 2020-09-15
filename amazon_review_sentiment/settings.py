23c9fceae707294d566f99a4dcb7ccec0de59da5
Vandenn <evan.livelo@gmail.com>
2020-09-15 18:02:47 +0800

    feat: Add a settings script

---
+++ amazon_review_sentiment/settings.py
@@ -0,0 +1,23 @@
+import os
+import pathlib
+
+SEED_VALUE = 42
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+DATA_FOLDER = os.path.join(PROJECT_ROOT, "data")
+EMBEDDINGS_FOLDER = os.path.join(PROJECT_ROOT, "embeddings")
+
+
+def data_path(path, *paths):
+    construct_path(DATA_FOLDER, path, *paths)
+
+
+def embeddings_path(path, *paths):
+    construct_path(EMBEDDINGS_FOLDER, path, *paths)
+
+
+def construct_path(folder_dir, path, *paths):
+    constructed_path = os.path.join(folder_dir, path, *paths)
+    directory = os.path.dirname(constructed_path)
+    pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+    return constructed_path
